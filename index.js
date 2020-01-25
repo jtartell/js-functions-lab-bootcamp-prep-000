14b08c01b27c9e3eece4cfc4299f065065970fcd
Jamie Tartell <jetartell@gmail.com>
2020-01-25 21:35:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
- describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-      expect(holidayCountdown("Father's Day", 120)).toEqual("It's 120 days until Father's Day!")
-    })
-  })
-})
